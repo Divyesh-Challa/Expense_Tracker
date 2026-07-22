16c9568e494735fdb9b926d1239fa59509682cb1
Divyesh Challa <divyeshchallavgr@gmail.com>
2026-07-21 22:05:47 -0700

    Day 6: Handled POST request to store form inputs in SQL and rendered expenses table

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask, render_template
-
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+def get_db_connection():
+    
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row  
+    return conn
 
 @app.route('/')
 def home():
+    
+    conn = get_db_connection()
+    expenses = conn.execute('SELECT * FROM expenses').fetchall()
+    conn.close()
+    
+    
+    return render_template('index.html', expenses=expenses)
+
+@app.route('/add', methods=['POST'])
+def add_expense():
+    
+    item_name = request.form['item']
+    item_amount = request.form['amount']
+    
+    
+    conn = get_db_connection()
+    conn.execute('INSERT INTO expenses (item, amount) VALUES (?, ?)',
+                 (item_name, item_amount))
+    conn.commit()
+    conn.close()
+    
    
-    return render_template('index.html')
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
